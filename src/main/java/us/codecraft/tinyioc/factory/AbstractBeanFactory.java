cbb2520fe2c094d3a9f5de906e338de34fe37b6c
gaoyanmeng <gaoyanmeng@ainfinit.com>
2021-10-13 17:36:07 +0800

    add comment

--- src/main/java/us/codecraft/tinyioc/factory/AbstractBeanFactory.java
+++ src/main/java/us/codecraft/tinyioc/factory/AbstractBeanFactory.java
@@ -10,14 +10,14 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public abstract class AbstractBeanFactory implements BeanFactory {
 
-	private Map<String, BeanDefinition> beanDefinitionMap = new ConcurrentHashMap<String, BeanDefinition>();
+    private Map<String, BeanDefinition> beanDefinitionMap = new ConcurrentHashMap<String, BeanDefinition>();
 
-	@Override
+    @Override
     public Object getBean(String name) {
-		return beanDefinitionMap.get(name).getBean();
-	}
+        return beanDefinitionMap.get(name).getBean();
+    }
 
-	@Override
+    @Override
     public void registerBeanDefinition(String name, BeanDefinition beanDefinition) throws Exception {
         Object bean = doCreateBean(beanDefinition);
         beanDefinition.setBean(bean);
@@ -26,6 +26,7 @@ public abstract class AbstractBeanFactory implements BeanFactory {
 
     /**
      * 初始化bean
+     *
      * @param beanDefinition
      * @return
      */
--- src/test/java/us/codecraft/tinyioc/BeanFactoryTest.java
+++ src/test/java/us/codecraft/tinyioc/BeanFactoryTest.java
@@ -9,26 +9,26 @@ import us.codecraft.tinyioc.factory.BeanFactory;
  */
 public class BeanFactoryTest {
 
-	@Test
-	public void test() throws Exception {
-		// 1.初始化beanfactory
-		BeanFactory beanFactory = new AutowireCapableBeanFactory();
+    @Test
+    public void test() throws Exception {
+        // 1.初始化beanfactory
+        BeanFactory beanFactory = new AutowireCapableBeanFactory();
 
-		// 2.bean定义
-		BeanDefinition beanDefinition = new BeanDefinition();
-		beanDefinition.setBeanClassName("us.codecraft.tinyioc.HelloWorldService");
+        // 2.bean定义
+        BeanDefinition beanDefinition = new BeanDefinition();
+        beanDefinition.setBeanClassName("us.codecraft.tinyioc.HelloWorldService");
 
-		// 3.设置属性
-		PropertyValues propertyValues = new PropertyValues();
-		propertyValues.addPropertyValue(new PropertyValue("text", "Hello World!"));
+        // 3.设置属性
+        PropertyValues propertyValues = new PropertyValues();
+        propertyValues.addPropertyValue(new PropertyValue("text", "Hello World!"));
         beanDefinition.setPropertyValues(propertyValues);
 
-		// 4.生成bean
-		beanFactory.registerBeanDefinition("helloWorldService", beanDefinition);
+        // 4.生成bean
+        beanFactory.registerBeanDefinition("helloWorldService", beanDefinition);
 
-		// 5.获取bean
-		HelloWorldService helloWorldService = (HelloWorldService) beanFactory.getBean("helloWorldService");
-		helloWorldService.helloWorld();
+        // 5.获取bean
+        HelloWorldService helloWorldService = (HelloWorldService) beanFactory.getBean("helloWorldService");
+        helloWorldService.helloWorld();
 
-	}
+    }
 }
