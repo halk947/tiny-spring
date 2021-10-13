3e6540a1c7a84440e9fd733291b54238c8a16827
gaoyanmeng <gaoyanmeng@ainfinit.com>
2021-10-13 17:17:16 +0800

    添加注释

--- src/main/java/us/codecraft/tinyioc/BeanDefinition.java
+++ src/main/java/us/codecraft/tinyioc/BeanDefinition.java
@@ -2,6 +2,9 @@ package us.codecraft.tinyioc;
 
 /**
  * @author yihua.huang@dianping.com
+ * <p>
+ * >>>
+ * 使用BeanDefinition来封装了bean对象，这样可以保存一些额外的元信息
  */
 public class BeanDefinition {
 
--- src/main/java/us/codecraft/tinyioc/BeanFactory.java
+++ src/main/java/us/codecraft/tinyioc/BeanFactory.java
@@ -8,14 +8,15 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class BeanFactory {
 
-	private Map<String, BeanDefinition> beanDefinitionMap = new ConcurrentHashMap<String, BeanDefinition>();
+    //>>>将实例保存在Map中
+    private Map<String, BeanDefinition> beanDefinitionMap = new ConcurrentHashMap<String, BeanDefinition>();
 
-	public Object getBean(String name) {
-		return beanDefinitionMap.get(name).getBean();
-	}
+    public Object getBean(String name) {
+        return beanDefinitionMap.get(name).getBean();
+    }
 
-	public void registerBeanDefinition(String name, BeanDefinition beanDefinition) {
-		beanDefinitionMap.put(name, beanDefinition);
-	}
+    public void registerBeanDefinition(String name, BeanDefinition beanDefinition) {
+        beanDefinitionMap.put(name, beanDefinition);
+    }
 
 }
