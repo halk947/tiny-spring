10820b1c740710df37b6b10999289c21491adad3
gaoyanmeng <gaoyanmeng@ainfinit.com>
2021-10-14 15:30:20 +0800

    add comment

--- src/main/java/us/codecraft/tinyioc/beans/factory/AutowireCapableBeanFactory.java
+++ src/main/java/us/codecraft/tinyioc/beans/factory/AutowireCapableBeanFactory.java
@@ -8,21 +8,21 @@ import java.lang.reflect.Field;
 
 /**
  * 可自动装配内容的BeanFactory
- * 
+ *
  * @author yihua.huang@dianping.com
  */
 public class AutowireCapableBeanFactory extends AbstractBeanFactory {
 
-	protected void applyPropertyValues(Object bean, BeanDefinition mbd) throws Exception {
-		for (PropertyValue propertyValue : mbd.getPropertyValues().getPropertyValues()) {
-			Field declaredField = bean.getClass().getDeclaredField(propertyValue.getName());
-			declaredField.setAccessible(true);
-			Object value = propertyValue.getValue();
-			if (value instanceof BeanReference) {
-				BeanReference beanReference = (BeanReference) value;
-				value = getBean(beanReference.getName());
-			}
-			declaredField.set(bean, value);
-		}
-	}
+    protected void applyPropertyValues(Object bean, BeanDefinition mbd) throws Exception {
+        for (PropertyValue propertyValue : mbd.getPropertyValues().getPropertyValues()) {
+            Field declaredField = bean.getClass().getDeclaredField(propertyValue.getName());
+            declaredField.setAccessible(true);
+            Object value = propertyValue.getValue();
+            if (value instanceof BeanReference) {
+                BeanReference beanReference = (BeanReference) value;
+                value = getBean(beanReference.getName());
+            }
+            declaredField.set(bean, value);
+        }
+    }
 }
